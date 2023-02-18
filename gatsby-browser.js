bde9e45fe0c2d76ec591e58526020ca7f3c561ab
Rafał Wichowski <rawic.me@gmail.com>
2023-02-18 13:16:54 +0100

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
-import { AnimatePresence } from 'framer-motion';
-import React from 'react';
+// import { AnimatePresence } from 'framer-motion';
+// import React from 'react';
 
-export const wrapPageElement = ({ element }) => (
-    <AnimatePresence initial={false} mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
-        {element}
-    </AnimatePresence>
-);
+// export const wrapPageElement = ({ element }) => (
+//     <AnimatePresence initial={false} mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
+//         {element}
+//     </AnimatePresence>
+// );
